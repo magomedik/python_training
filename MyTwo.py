b064dbb2b6efffa771b48b64103a62ffa500a965
Magomed <58663516+magomedik@users.noreply.github.com>
2020-10-12 12:23:18 +0300

    first project pass

--- MyTwo.py
+++ MyTwo.py
@@ -7,7 +7,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class 1(unittest.TestCase):
+class MyTwo(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -15,19 +15,13 @@ class 1(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_1(self):
+    def test_my_two(self):
         driver = self.driver
-        driver.get("https://www.citrus.ua/")
-        driver.find_element_by_xpath("//header[@id='header']/div[3]/div/div/div[2]/div").click()
-        driver.find_element_by_xpath("//header[@id='header']/div[3]/div/div/div[2]/div[2]/ul/li/a/span[2]").click()
-        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_1 | ]]
-        driver.find_element_by_xpath("//img[contains(@src,'https://static.tildacdn.com/tild3839-6162-4331-b862-326435373731/Vector_Smart_Object.svg')]").click()
-        driver.find_element_by_xpath("//img[@alt='Realme']").click()
-        driver.find_element_by_link_text(u"Наушники").click()
-        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_2 | ]]
-        driver.find_element_by_id("search-input").clear()
-        driver.find_element_by_id("search-input").send_keys(u"привет")
-        driver.find_element_by_id("search-input").send_keys(Keys.ENTER)
+        driver.get("https://rozetka.com.ua/")
+        driver.find_element_by_name("search").click()
+        driver.find_element_by_name("search").clear()
+        driver.find_element_by_name("search").send_keys("hello")
+        driver.find_element_by_name("search").send_keys(Keys.ENTER)
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
